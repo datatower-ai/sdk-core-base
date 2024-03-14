2ec14dfb1362500e3f1dc1e015850a3540a18a38
linkailong <HoiLungLam@outlook.com>
2024-03-14 12:12:57 +0800

    Added aarch64-apple-darwin to build_lua.sh

--- build_lua.sh
+++ build_lua.sh
@@ -1,9 +1,20 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
+
+#
+# Build Lua
+#
 mkdir -p "$BASEDIR/output/lua/"
 
-cargo rustc --release --package lua --features lua54 -- -C link-arg=-undefined -C link-arg=dynamic_lookup
-mv "$BASEDIR/target/release/libdt_core_lua.dylib" "$BASEDIR/output/lua/dt_core_lua-lua54-x86_64-macos.so"
+cargo rustc --release --package lua --features lua54
+cp -f "$BASEDIR/target/release/libdt_core_lua.dylib" "$BASEDIR/output/lua/dt_core_lua-lua54-x86_64-macos.so"
+
+cargo rustc --release --package lua --features lua54 --target aarch64-apple-darwin
+cp -f "$BASEDIR/target/aarch64-apple-darwin/release/libdt_core_lua.dylib" "$BASEDIR/output/lua/dt_core_lua-lua54-aarch64-macos.so"
 
 cargo rustc --release --package lua --features lua54 --target x86_64-unknown-linux-gnu
-mv "$BASEDIR/target/x86_64-unknown-linux-gnu/release/libdt_core_lua.so" "$BASEDIR/output/lua/dt_core_lua-lua54-x86_64-linux-gnu.so"
+cp -f "$BASEDIR/target/x86_64-unknown-linux-gnu/release/libdt_core_lua.so" "$BASEDIR/output/lua/dt_core_lua-lua54-x86_64-linux-gnu.so"
+
+#
+# Build xxx
+#
