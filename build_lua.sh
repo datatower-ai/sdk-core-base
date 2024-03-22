d32b81e0c30da93c88283114631ba5a58316039e
linkailong <HoiLungLam@outlook.com>
2024-03-22 14:33:42 +0800

    separate build script for lua and python

--- build_lua.sh
+++ build_lua.sh
@@ -34,44 +34,9 @@ function build_lua() {
 #  cp -f "$BASEDIR/target/aarch64-pc-windows-msvc/release/libdt_core_lua.so" "$BASEDIR/output/lua/dt_core_lua-$1-windows-aarch64.so"
 }
 
-####################################
-# Build Python
-####################################
-function build_python() {
-  mkdir -p "$BASEDIR/output/python/"
-  cd python || (echo "Failed to \`cd python\`" & exit)
-  source .env/bin/activate
-
-  function find_name_4_whl() {
-    find ../target/wheels/ -name "dt_core_python-*$1*$2*.whl" -type f -exec basename {} \; | head -1
-  }
-
-  maturin build --release --zig --interpreter python3.9 --target x86_64-apple-darwin
-  cp -f "../target/wheels/$(find_name_4_whl macos x86_64)" "../output/python/dt_core_python-macos-x86_64.whl"
-
-  maturin build --release --zig --interpreter python3.9 --target aarch64-apple-darwin
-  cp -f "../target/wheels/$(find_name_4_whl macos arm64)" "../output/python/dt_core_python-macos-arm64.whl"
-
-  maturin build --release --zig --interpreter python3.9 --target x86_64-unknown-linux-gnu
-  cp -f "../target/wheels/$(find_name_4_whl manylinux x86_64)" "../output/python/dt_core_python-linux-x86_64.whl"
-
-  maturin build --release --zig --interpreter python3.9 --target aarch64-unknown-linux-gnu
-  cp -f "../target/wheels/$(find_name_4_whl manylinux aarch64)" "../output/python/dt_core_python-linux-aarch64.whl"
-
-  maturin build --release --zig --interpreter python3.9 --target x86_64-pc-windows-msvc
-  cp -f "../target/wheels/$(find_name_4_whl win amd64)" "../output/python/dt_core_python-windows-amd64.whl"
-
-  maturin build --release --zig --interpreter python3.9 --target aarch64-pc-windows-msvc
-  cp -f "../target/wheels/$(find_name_4_whl win arm64)" "../output/python/dt_core_python-windows-arm64.whl"
-
-  deactivate
-  cd "../"
-}
-
 
 ####################################
 # Build
 ####################################
 build_lua lua54 &
-# build_python &
 wait
