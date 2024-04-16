a81a5f40d2d3f617d4975096baa7237ccb262d78
linkailong <HoiLungLam@outlook.com>
2024-04-16 15:24:29 +0800

    Added GitHub CI

--- .github/scripts/version_check.sh
+++ .github/scripts/version_check.sh
@@ -1,7 +1,7 @@
 ver_common=$(grep -oE "^version = \".*\"$" "./common/Cargo.toml" | sed -ne "s/version = \"\(.*\)\"$/\1/p")
 echo "Common: $ver_common"
 
-failed=false
+failed=0
 
 # $1: version, $2: name
 function check() {
@@ -9,17 +9,36 @@ function check() {
       echo "✔ $2: $1"
     else
       echo "✘ $2: $1"
-      failed=true
+      failed=$((failed+1))
     fi
 }
 
+function check_nodejs_version() {
+  ver=$(grep -oE "^ *\"version\": \".*\", *$" "./nodejs/package.json" | sed -ne "s/.*\"version\": \"\(.*\)\",.*/\1/p")
+  check "$ver" "Node.js"
+
+  find ./nodejs/npm/ -maxdepth 2 -type f -name "package.json" | while read fpath; do
+    ver=$(cat $fpath | grep -oE "^ *\"version\": \".*\", *$" | sed -ne "s/.*\"version\": \"\(.*\)\",.*/\1/p")
+    check "$ver" "  ◇ $(echo $fpath | sed -n "s/^.*\/\(.*\)\/package.json$/\1/p")"
+  done
+}
+
+
+# Python
 ver_python=$(grep -oE "^version = .*$" "./python/Cargo.toml" | sed -ne "s/^version = \"\(.*\)\" *$/\1/p")
 check "$ver_python" "Python"
 
+# Java
 ver_java=$(grep -oE "^version = .*$" "./java/java/lib/build.gradle" | sed -ne "s/^version = \"\(.*\)\"$/\1/p")
 check "$ver_java" "Java"
 
+# Node.js
+check_nodejs_version
 
-if [ "$failed" = true ]; then
-  exit 1
-fi
+# Golang: ignored
+
+# Lua
+# ...
+
+
+exit $((failed))
